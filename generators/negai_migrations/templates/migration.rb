266eb947feeebdade0a806726b759311f7a59af9
Dario Seminara <rseminara@hotmail.com>
2010-06-07 18:26:10 -0300

    completed database tables in the negai migration

--- generators/negai_migrations/templates/migration.rb
+++ generators/negai_migrations/templates/migration.rb
@@ -1,3 +1,21 @@
 class CreateNegaiObjects < ActiveRecord::Migration
+  def self.up
+    create_table :scripts do |t|
+      t.string :name
+      t.string :content
 
+      t.timestamps
+    end
+
+    create_table :executions do |t|
+      t.belongs_to :script  # script to execute
+      t.string :output  # standard output of the execution
+      t.integer :execution_time
+      t.timestamps
+    end
+  end
+  def self.down
+    drop_table :executions
+    drop_table :scripts
+  end
 end
